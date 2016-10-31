dc9b3c1ee39e47c243d51f08e4b086ec61cefd19
jason chen <cjx89807@alibaba-inc.com>
2016-10-31 19:51:08 +0800

    refactor: 修改demo重构

--- webgl/webgl/points3/js/script.js
+++ webgl/webgl/points3/js/script.js
@@ -1,6 +1,6 @@
 var lastTime = 0;
 var vendors = ['ms', 'moz', 'webkit', 'o'];
-for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
     window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
     window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
         window[vendors[x] + 'CancelRequestAnimationFrame'];
@@ -10,7 +10,7 @@ if (!window.requestAnimationFrame) {
         var currTime = new Date().getTime();
         var timeToCall = Math.max(0, 16 - (currTime - lastTime));
         var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-          timeToCall);
+            timeToCall);
         lastTime = currTime + timeToCall;
         return id;
     };
@@ -22,306 +22,181 @@ if (!window.cancelAnimationFrame) {
 }
 
 var w = window.innerWidth,
-	h = window.innerHeight,
-	gl;
+    h = window.innerHeight,
+    gl;
 
-onload = function(){
+onload = function() {
+    var c = document.getElementById('canvas');
+    c.width = w;
+    c.height = h;
 
-	var c = document.getElementById('canvas');  
-	c.width = w;  
-	c.height = h;  
+    // 获取canvas的webgl绘图上下文 gl
+    gl = c.getContext('webgl') || c.getContext('experimental-webgl');
 
-	gl = c.getContext('webgl') || c.getContext('experimental-webgl');
-	
-	var v_shader = create_shader('vs');
-	var f_shader = create_shader('fs');
+    // 编译着色器程序
+    var prg = compileProgram(document.getElementById('vs').text, document.getElementById('fs').text);
 
-	var v_shader2 = create_shader('vs2');
-	var f_shader2 = create_shader('fs2');
-	
-	var prg = create_program(v_shader, f_shader);
-	var prg2 = create_program(v_shader2, f_shader2);
-	
-	var attLocation = new Array();
-	prg_position = gl.getAttribLocation(prg, 'position');
-	prg_size = gl.getAttribLocation(prg, 'size');
-	
-	
-	var torusData = createDNA(8);// createPoint(50000);	// torus(64, 64, 1.5, 3.0);
-	var position = torusData.p;
-	var LL = position.length/3;
-	var size = torusData.s;
-	// var index = torusData.i;
-	
-	var pos_vbo = create_vbo(position);
-	var nor_vbo = create_vbo(size);
-	
-	set_attribute([pos_vbo, nor_vbo], [prg_position, prg_size], [3,1]);
-	
-	// var ibo = create_ibo(index);
-	
-	// gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-	var mvpM = gl.getUniformLocation(prg, 'mvpMatrix');
-	var mvpM2 = gl.getUniformLocation(prg2, 'mvpMatrix');
-	
-	
-	var m = new Matrix();
-	
-	var mMatrix = m.identity(m.create());
-	var vMatrix = m.identity(m.create());
-	var pMatrix = m.identity(m.create());
-	var tmpMatrix = m.identity(m.create());
-	var mvpMatrix = m.identity(m.create());
-	var invMatrix = m.identity(m.create());
-	
-	var lightDirection = [-0.5, 1.0, 0.5];
-	
-	var ambientColor = [0.1, 0.1, 0.1, 1.0];
-	
-	var eyeDirection = [0.0, 0.0, 8.0];
-
-	
-	
-	var count = 0,
-		deep = 0.05;
-
-	gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-	gl.enable(gl.BLEND);
-
-	var PRG = prg,
-		MVP = mvpM,
-		btn = true;
-	document.onclick = function(){
-		if(btn){
-			PRG = prg2;
-			MVP = mvpM2;
-			btn = false;
-		}else{
-			PRG = prg;
-			MVP = mvpM;
-			btn = true;
-		}
-	};
-	
-	(function(){
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		gl.clearDepth(1.0);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		
-		// eyeDirection[2] = eyeDirection[2] - deep;
-		// if(eyeDirection[2]<2)deep=-0.1;
-		// if(eyeDirection[2]>32)deep=0.1;
-
-		gl.useProgram(PRG);
-
-		count = (0.01+count)%360;
-
-		m.lookAt(eyeDirection, [0, 0, 0], [0, 1, 0], vMatrix);
-		m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
-		m.multiply(pMatrix, vMatrix, tmpMatrix);
-
-		m.identity(mMatrix);
-		// m.rotate(mMatrix, count, [1,1,0], mMatrix);
-		m.rotate(mMatrix, count, [1,0,0], mMatrix);
-
-		m.multiply(tmpMatrix, mMatrix, mvpMatrix);
-		gl.uniformMatrix4fv(MVP, false, mvpMatrix);
+    // 获取着色器持续里面的变量指针
+    var prg_position = gl.getAttribLocation(prg, 'position');
+    var prg_color = gl.getAttribLocation(prg, 'color');
+    var prg_size = gl.getAttribLocation(prg, 'size');
+    var prg_viewMatrix = gl.getUniformLocation(prg, 'viewMatrix');
+    var prg_perspectiveMatrix = gl.getUniformLocation(prg, 'perspectiveMatrix');
+    var prg_modelMatrix = gl.getUniformLocation(prg, 'modelMatrix');
 
 
-		gl.drawArrays(gl.POINTS, 0, LL);
-		
-		
-		gl.flush();
-		
-		requestAnimationFrame(arguments.callee);
-	})();
-	
+    // 利用数学公式生成DNA
+    var cubeData = createPoint(8);
+    var position = cubeData.p;
+    var size = cubeData.s;
+    var color = cubeData.c;
+
+    // 利用生成的顶点信息生成将要上传给GPU的绘制的坐标数据缓冲区对象
+    var pos_vbo = createVBO(position);
+    var size_vbo = createVBO(size);
+    var color_vbo = createVBO(color);
+
+    // 将数据缓冲区对象上传到着色器指针所指向的内存中
+    bindAttribute([pos_vbo, color_vbo, size_vbo], [prg_position, prg_color, prg_size], [3, 3, 1]);
+
+
+    var m = new Matrix();
+
+    var modelPosition = new Point();
+    var tmpMatrix = m.identity(m.create());
+    var modelMatrix = m.identity(m.create());
+    var viewMatrix = m.identity(m.create());
+    var perspectiveMatrix = m.identity(m.create());
+
+    var eyeDirection = [0.0, 0.0, 8.0];
 
 
 
-	function create_shader(id){
-		var shader;
-		
-		var scriptElement = document.getElementById(id);
-		
-		if(!scriptElement){return;}
-		
-		switch(scriptElement.type){
-			
-			case 'x-shader/x-vertex':
-				shader = gl.createShader(gl.VERTEX_SHADER);
-				break;
-				
-			case 'x-shader/x-fragment':
-				shader = gl.createShader(gl.FRAGMENT_SHADER);
-				break;
-			default :
-				return;
-		}
-		
-		gl.shaderSource(shader, scriptElement.text);
-		
-		gl.compileShader(shader);
-		
-		if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-			
-			return shader;
-		}else{
-			
-			alert(gl.getShaderInfoLog(shader));
-		}
-	}
-	
-	function create_program(vs, fs){
-		var program = gl.createProgram();
-		
-		gl.attachShader(program, vs);
-		gl.attachShader(program, fs);
-		
-		gl.linkProgram(program);
-		
-		if(gl.getProgramParameter(program, gl.LINK_STATUS)){
-		
-			//gl.useProgram(program);
-			
-			return program;
-		}else{
-			
-			alert(gl.getProgramInfoLog(program));
-		}
-	}
-	
-	function create_vbo(data){
-		var vbo = gl.createBuffer();
-		
-		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-		
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-		
-		gl.bindBuffer(gl.ARRAY_BUFFER, null);
-		
-		return vbo;
-	}
-	
-	function set_attribute(vbo, attL, attS){
-		for(var i in vbo){
-			gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
-			
-			gl.enableVertexAttribArray(attL[i]);
-			
-			gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
-		}
-	}
-	
-	function create_ibo(data){
-		var ibo = gl.createBuffer();
-		
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-		
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
-		
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-		
-		return ibo;
-	}
-	
-	function torus(row, column, irad, orad, color){
-		var pos = new Array(), nor = new Array(),
-		    col = new Array(), idx = new Array();
-		for(var i = 0; i <= row; i++){
-			var r = Math.PI * 2 / row * i;
-			var rr = Math.cos(r);
-			var ry = Math.sin(r);
-			for(var ii = 0; ii <= column; ii++){
-				var tr = Math.PI * 2 / column * ii;
-				var tx = (rr * irad + orad) * Math.cos(tr);
-				var ty = ry * irad;
-				var tz = (rr * irad + orad) * Math.sin(tr);
-				var rx = rr * Math.cos(tr);
-				var rz = rr * Math.sin(tr);
-				if(color){
-					var tc = color;
-				}else{
-					tc = hsva(360 / column * ii, 1, 1, 1);
-				}
-				pos.push(tx, ty, tz);
-				nor.push(rx, ry, rz);
-				col.push(tc[0], tc[1], tc[2], tc[3]);
-			}
-		}
-		for(i = 0; i < row; i++){
-			for(ii = 0; ii < column; ii++){
-				r = (column + 1) * i + ii;
-				idx.push(r, r + column + 1, r + 1);
-				idx.push(r + column + 1, r + column + 2, r + 1);
-			}
-		}
-		return {p : pos, n : nor, c : col, i : idx};
-	}
-	
-	function hsva(h, s, v, a){
-		if(s > 1 || v > 1 || a > 1){return;}
-		var th = h % 360;
-		var i = Math.floor(th / 60);
-		var f = th / 60 - i;
-		var m = v * (1 - s);
-		var n = v * (1 - s * f);
-		var k = v * (1 - s * (1 - f));
-		var color = new Array();
-		if(!s > 0 && !s < 0){
-			color.push(v, v, v, a); 
-		} else {
-			var r = new Array(v, n, m, m, k, v);
-			var g = new Array(k, v, v, n, m, m);
-			var b = new Array(m, m, k, v, v, n);
-			color.push(r[i], g[i], b[i], a);
-		}
-		return color;
-	}
+    var count = 0;
 
-	function createPoint(num){
-		var pos = new Array(),size = new Array(),radius = 6.0,toRAD = Math.PI/180.0;
-
-		for(var i=0;i<num;i++){
-			var degX = toRAD*randomRange(0,360),
-				degY = toRAD*randomRange(0,360),
-				degZ = toRAD*randomRange(0,360),
-				x = Math.sin(degX)*Math.cos(degY)*radius,
-				y = Math.sin(degX)*Math.sin(degY)*radius,
-				z = Math.cos(degX)*radius,
-				s = Math.random()*2.0;
-			pos.push(x,y,z);
-			size.push(s);
-		}
-		console.log(x+','+y+','+z);
-		return {p: pos, s: size};
-	}
+    gl.blendFunc(gl.SRC_COLOR, gl.DST_COLOR);
+    gl.enable(gl.BLEND);
 
 
+    (function() {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        m.lookAt(eyeDirection, [0, 0, 0], [0, 1, 0], viewMatrix);
+        m.perspective(45, c.width / c.height, 0.1, 1000, perspectiveMatrix);
 
-	function createDNA(zIndex){
-		var pos = new Array(),size = new Array(),radius = 6.0,toRAD = Math.PI/180.0,
-			degX = toRAD*randomRange(0,360),
-			degY = toRAD*randomRange(0,360),
-			degZ = toRAD*randomRange(0,360);
+        count = (0.01 + count) % 360;
 
-		for(var i=-zIndex;i<zIndex;i+=0.01){
-			var rad = i*80*toRAD;
-			for(var ii=-20;ii<20;ii+=0.6){
-				var r = ii*radius/40,
-					y = Math.cos(rad)*r,
-					z = -Math.sin(rad)*r,
-					x = i*5,
-					s = Math.random()*4.0;
-					pos.push(x,y,z);
-					size.push(s);
-			}
-		}
-			console.log(x+','+y+','+z);
-		return {p: pos, s: size};
-	}
+        m.identity(modelMatrix);
+        m.rotate(modelMatrix, count, [1, 0, 0], modelMatrix);
 
-	function randomRange(LLimit,TLimit){
-	    return Math.floor(Math.random()*(TLimit-LLimit) + LLimit);
-	};
+        gl.uniformMatrix4fv(prg_modelMatrix, false, modelMatrix);
+
+        gl.uniformMatrix4fv(prg_viewMatrix, false, viewMatrix);
+        gl.uniformMatrix4fv(prg_perspectiveMatrix, false, perspectiveMatrix);
+        gl.drawArrays(gl.POINTS, 0, size.length);
+        gl.flush();
+
+        requestAnimationFrame(arguments.callee);
+    })();
+
 };
+
+
+
+function Point() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+}
+
+
+
+function compileProgram(vs, fs) {
+    var vShader, fShader;
+
+    vShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vShader, vs);
+    gl.compileShader(vShader);
+
+    fShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fShader, fs);
+    gl.compileShader(fShader);
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vShader);
+    gl.attachShader(program, fShader);
+    gl.linkProgram(program);
+
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        gl.useProgram(program);
+        return program;
+    } else {
+        alert(gl.getProgramInfoLog(program));
+    }
+}
+
+function createVBO(data) {
+    var vbo = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    return vbo;
+}
+
+function bindAttribute(vbo, attL, attS) {
+    for (var i in vbo) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+
+        gl.enableVertexAttribArray(attL[i]);
+
+        gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+    }
+}
+
+function createPoint(zIndex) {
+    var pos = new Array(),
+        size = new Array(),
+        color = new Array(),
+        radius = 6.0,
+        toRAD = Math.PI / 180.0;
+
+    for (var i = -zIndex; i < zIndex; i += 0.01) {
+        var rad = i * 80 * toRAD;
+        for (var ii = -20; ii < 20; ii += 0.6) {
+            var r = ii * radius / 40,
+                y = Math.cos(rad) * r,
+                z = -Math.sin(rad) * r,
+                x = i * 5,
+                s = Math.random() * 4.0,
+            		rgb = setHex(random(COLOURS));
+            pos.push(x, y, z);
+            size.push(s);
+            color.push(rgb.r, rgb.g, rgb.b);
+        }
+    }
+
+    return { p: pos, s: size, c: color };
+}
+
+function randomRange(LLimit, TLimit) {
+    return Math.floor(Math.random() * (TLimit - LLimit) + LLimit);
+};
+
+function random(arr) {
+    return arr[~~(Math.random() * arr.length)];
+}
+
+function setHex(hex) {
+    hex = Math.floor(hex);
+    var out = {};
+    out.r = (hex >> 16 & 255) / 255;
+    out.g = (hex >> 8 & 255) / 255;
+    out.b = (hex & 255) / 255;
+    return out;
+}
+var COLOURS = [0x69D2E7, 0xA7DBD8, 0xE0E4CC, 0xF38630, 0xFA6900, 0xFF4E50, 0xF9D423];
