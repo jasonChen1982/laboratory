fee2acf65fa5493d110cdca5f59a69e46943c3b5
jason chen <cjx89807@alibaba-inc.com>
2016-12-05 00:03:43 +0800

    feat: 配色

--- webgl/earth/js/cloud.js
+++ webgl/earth/js/cloud.js
@@ -1,39 +1,39 @@
 function Cloud(){
-	// Create an empty container that will hold the different parts of the cloud
-	this.mesh = new THREE.Object3D();
+	this.mesh = new THREE.Group();
 	
-	// create a cube geometry;
-	// this shape will be duplicated to create the cloud
-	var geom = new THREE.BoxGeometry(0.6,0.6,0.6);
+	var geom = new THREE.BoxGeometry(0.2,0.2,0.2);
 	
-	// create a material; a simple white material will do the trick
 	var mat = new THREE.MeshPhongMaterial({
 		color: new THREE.Color(0xffffff),  
 	});
 	
-	// duplicate the geometry a random number of times
-	var nBlocs = 3+Math.floor(Math.random()*3);
+	var nBlocs = 100+(Math.random()*20 >> 0);
 	for (var i=0; i<nBlocs; i++ ){
 		
-		// create the mesh by cloning the geometry
 		var m = new THREE.Mesh(geom, mat); 
 		
-		// set the position and the rotation of each cube randomly
-		m.position.x = i*8;
-		m.position.y = Math.random()*5;
-		m.position.z = Math.random()*5;
+        var degX = toRAD * randomRange(50, 130),
+            degY = toRAD * randomRange(0, 360),
+			radius = randomRange(3.5, 4),
+            x = Math.sin(degX) * Math.cos(degY) * radius,
+            y = Math.cos(degX) * radius,
+            z = Math.sin(degX) * Math.sin(degY) * radius;
+		m.position.x = x;
+		m.position.y = y;
+		m.position.z = z;
 		m.rotation.z = Math.random()*Math.PI*2;
 		m.rotation.y = Math.random()*Math.PI*2;
 		
-		// set the size of the cube randomly
-		var s = .1 + Math.random()*.9;
+		var s = .3 + Math.random()*.7;
 		m.scale.set(s,s,s);
 		
-		// allow each cube to cast and to receive shadows
 		m.castShadow = true;
 		m.receiveShadow = true;
 		
-		// add the cube to the container we first created
 		this.mesh.add(m);
 	} 
 }
+var toRAD = Math.PI / 180.0;
+function randomRange(LLimit, TLimit) {
+    return (Math.random() * (TLimit - LLimit) + LLimit);
+};
