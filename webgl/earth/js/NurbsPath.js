214279a8c7f14f209ebafec15385a24df4e1f393
jason chen <cjx89807@alibaba-inc.com>
2016-12-12 20:06:33 +0800

    feat: 增加效果

--- webgl/earth/js/NurbsPath.js
+++ webgl/earth/js/NurbsPath.js
@@ -28,15 +28,23 @@ window.androidUC = /uc/i.test(window.navigator.userAgent) && /android/i.test(win
     }
     window.RAF = window.requestAnimFrame = window.requestAnimationFrame;
 })();
-// lookat NURBS curve
 
-var lookControlPoints = [];
-var lookKnots = [];
-var lookDegree = 3;
-
-for ( var i = 0; i <= lookDegree; i ++ ) {
-    lookKnots.push( 0 );
+function CreatePath(control, degree){
+    this.control = control;
+    this.knots = [];
+    this.degree = degree || 3;
+    this.init();
 }
+CreatePath.prototype.init = function(){
+    for ( var i = 0; i <= this.degree; i ++ ) {
+        this.knots.push( 0 );
+    }
+    for ( var i = 0, j = this.control.length; i < j; i ++ ) {
+        var knot = ( i + 1 ) / ( j - this.degree );
+        this.knots.push( THREE.Math.clamp( knot, 0, 1 ) );
+    }
+    this.nurbs = new THREE.NURBSCurve(this.degree, this.knots, this.control);
+};
 
 var lookControl = [
     new THREE.Vector4(   1, 1.5,-1.6, 1),
@@ -51,30 +59,7 @@ var lookControl = [
     new THREE.Vector4(   0,   1,   0, 1),
     new THREE.Vector4(   0,   0,   0, 1),
 ];
-
-for ( var i = 0, j = lookControl.length; i < j; i ++ ) {
-
-    lookControlPoints.push(
-        lookControl[i]
-    );
-
-    var knot = ( i + 1 ) / ( j - lookDegree );
-    lookKnots.push( THREE.Math.clamp( knot, 0, 1 ) );
-
-}
-
-var lookCurve = new THREE.NURBSCurve(lookDegree, lookKnots, lookControlPoints);
-
-
-// fog NURBS curve
-
-var fovControlPoints = [];
-var fovKnots = [];
-var fovDegree = 3;
-
-for ( var i = 0; i <= fovDegree; i ++ ) {
-    fovKnots.push( 0 );
-}
+var lookCurve = new CreatePath(lookControl);
 
 var fovControl = [
     new THREE.Vector4(30, 0, 0, 1),
@@ -85,38 +70,16 @@ var fovControl = [
     new THREE.Vector4(60, 0, 0, 1),
     new THREE.Vector4(90, 0, 0, 1),
 ];
-
-for ( var i = 0, j = fovControl.length; i < j; i ++ ) {
-
-    fovControlPoints.push(
-        fovControl[i]
-    );
-
-    var knot = ( i + 1 ) / ( j - fovDegree );
-    fovKnots.push( THREE.Math.clamp( knot, 0, 1 ) );
-
-}
-
-var fovCurve = new THREE.NURBSCurve(fovDegree, fovKnots, fovControlPoints);
-
-// NURBS curve
-
-var nurbsControlPoints = [];
-var nurbsKnots = [];
-var nurbsDegree = 3;
-
-for ( var i = 0; i <= nurbsDegree; i ++ ) {
-    nurbsKnots.push( 0 );
-}
+var fovCurve = new CreatePath(fovControl);
 
 var cameraControl = [
     new THREE.Vector4(0.75,1.05,  -3, 1),
     new THREE.Vector4(   1, 0.4,-2.5, 1),
-    new THREE.Vector4(1.25,-0.5,-2.7, 1),
-    new THREE.Vector4(   0,  -1,  -3, 1),
-    new THREE.Vector4(  -1,  -1,  -3, 1),
-    new THREE.Vector4(  -2,-0.7,-2.5, 1),
-    new THREE.Vector4(  -3,-0.2,  -2, 1),
+    new THREE.Vector4(1.25,-0.2,-2.7, 1),
+    new THREE.Vector4(   0,-0.4,  -3, 1),
+    new THREE.Vector4(  -1,-0.4,  -3, 1),
+    new THREE.Vector4(  -2,-0.2,-2.5, 1),
+    new THREE.Vector4(  -3,-0.1,  -2, 1),
     new THREE.Vector4(-3.4,   0,-1.6, 1),
     new THREE.Vector4(-3.2, 0.4,  -1, 1),
     new THREE.Vector4(-2.6, 0.5, 0.3, 1),
@@ -127,22 +90,10 @@ var cameraControl = [
     new THREE.Vector4(-0.1,   0, 6.4, 1),
     new THREE.Vector4(   0,   0,   7, 1)
 ];
+var cameraCurve = new CreatePath(cameraControl);
 
-for ( var i = 0, j = cameraControl.length; i < j; i ++ ) {
-
-    nurbsControlPoints.push(
-        cameraControl[i]
-    );
-
-    var knot = ( i + 1 ) / ( j - nurbsDegree );
-    nurbsKnots.push( THREE.Math.clamp( knot, 0, 1 ) );
-
-}
-
-var nurbsCurve = new THREE.NURBSCurve(nurbsDegree, nurbsKnots, nurbsControlPoints);
-
-
-function NurbsPath() {
+function NurbsPath(option) {
+    option = option || {};
     this.tween = {
         linear: function (t, b, c, d){  //匀速
             return c*t/d + b;
@@ -154,6 +105,7 @@ function NurbsPath() {
             return -c/2 * ((--t)*(t-2) - 1) + b;
         }
     };
+    this.path = option.path;
 }
 NurbsPath.prototype.motion = function(c, d, e, f) {
     var g = Date.now(),
@@ -164,10 +116,16 @@ NurbsPath.prototype.motion = function(c, d, e, f) {
     function go() {
         var a = Date.now() - g,
             stop = a > c,
-            t;
-        if (stop) { t = 1 } else { t = This.tween[d](a, 0, 1, c) }
-        e && e(nurbsCurve.getPoint(t), t);
-        if (stop) { f && f() } else { window.RAF(go) } 
+            t = 1;
+        if (!stop) {
+            t = This.tween[d](a, 0, 1, c);
+        }
+        e && e(This.path.getPoint(t), t);
+        if (stop) { 
+            f && f();
+        } else {
+            window.RAF(go);
+        } 
     }
     window.RAF(go);
 };
