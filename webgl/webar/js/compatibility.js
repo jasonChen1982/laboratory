93b114ee4a2b82557002e30b630e900f1ab0ba56
jason chen <cjx89807@alibaba-inc.com>
2017-03-21 12:01:13 +0800

    feat: polyfill rtc

--- webgl/webar/js/compatibility.js
+++ webgl/webar/js/compatibility.js
@@ -34,7 +34,7 @@ var compatibility = (function() {
                                             clearTimeout(id);
                                         };
             return cancelAnimationFrame.call(window, id);
-        },
+        };
 
         // Older browsers might not implement mediaDevices at all, so we set an empty object first
         if (navigator.mediaDevices === undefined) {
@@ -45,26 +45,26 @@ var compatibility = (function() {
         // with getUserMedia as it would overwrite existing properties.
         // Here, we will just add the getUserMedia property if it's missing.
         if (navigator.mediaDevices.getUserMedia === undefined) {
-        navigator.mediaDevices.getUserMedia = function(constraints) {
+            navigator.mediaDevices.getUserMedia = function(constraints) {
 
-            // First get ahold of the legacy getUserMedia, if present
-            var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+                // First get ahold of the legacy getUserMedia, if present
+                var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-            // Some browsers just don't implement it - return a rejected promise with an error
-            // to keep a consistent interface
-            if (!getUserMedia) {
-            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                // Some browsers just don't implement it - return a rejected promise with an error
+                // to keep a consistent interface
+                if (!getUserMedia) {
+                return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                }
+
+                // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+                return new Promise(function(resolve, reject) {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+                });
             }
-
-            // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-            return new Promise(function(resolve, reject) {
-            getUserMedia.call(navigator, constraints, resolve, reject);
-            });
-        }
         }
 
 
-        detectEndian = function() {
+        var detectEndian = function() {
             var buf = new ArrayBuffer(8);
             var data = new Uint32Array(buf);
             data[0] = 0xff000000;
